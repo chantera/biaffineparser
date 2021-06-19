c4b31bbef65e8621a5524661645a304c16e06217
Hiroki Teranishi <hiroki.teranishi@riken.jp>
2021-06-20 03:30:08 +0900

    Refactor main.

--- src/main.py
+++ src/main.py
@@ -14,34 +14,57 @@ logger.addHandler(logging.StreamHandler())
 logger.setLevel(logging.INFO)
 
 
-def train(
-    train_file,
-    eval_file=None,
-    embed_file=None,
-    epochs=20,
-    batch_size=5000,
-    lr=2e-3,
-    cuda=False,
-    save_dir=None,
-    seed=None,
-):
-    if seed is not None:
-        utils.seed_everything(seed)
-    device = torch.device("cuda" if cuda else "cpu")
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest="command")
+
+    subparser = subparsers.add_parser("train")
+    subparser.set_defaults(command=train)
+    subparser.add_argument("--train_file", type=str, required=True, metavar="FILE")
+    subparser.add_argument("--eval_file", type=str, default=None, metavar="FILE")
+    subparser.add_argument("--embed_file", type=str, default=None, metavar="FILE")
+    subparser.add_argument("--epochs", type=int, default=300, metavar="NUM")
+    subparser.add_argument("--batch_size", type=int, default=5000, metavar="NUM")
+    subparser.add_argument("--lr", type=float, default=2e-3, metavar="VALUE")
+    subparser.add_argument("--cuda", action="store_true")
+    subparser.add_argument("--save_dir", type=str, default=None, metavar="DIR")
+    subparser.add_argument("--seed", type=int, default=None, metavar="VALUE")
+
+    subparser = subparsers.add_parser("evaluate")
+    subparser.set_defaults(command=evaluate)
+    subparser.add_argument("--eval_file", type=str, required=True, metavar="FILE")
+    subparser.add_argument("--checkpoint_file", "--ckpt", type=str, required=True, metavar="FILE")
+    subparser.add_argument(
+        "--preprocessor_file", "--proc", type=str, required=True, metavar="FILE"
+    )
+    subparser.add_argument("--batch_size", type=int, default=5000, metavar="NUM")
+    subparser.add_argument("--cuda", action="store_true")
+    subparser.add_argument("--verbose", "-v", action="store_true")
+
+    args = parser.parse_args()
+    args.command(args)
+
+
+def train(args):
+    if args.seed is not None:
+        utils.seed_everything(args.seed)
+    device = torch.device("cuda" if args.cuda else "cpu")
 
     preprocessor = Preprocessor()
-    preprocessor.build_vocab(train_file)
-    if embed_file:
-        preprocessor.load_embeddings(embed_file)
+    preprocessor.build_vocab(args.train_file)
+    if args.embed_file:
+        preprocessor.load_embeddings(args.embed_file)
     loader_config = dict(
         preprocessor=preprocessor,
-        batch_size=batch_size,
+        batch_size=args.batch_size,
         device=device,
     )
-    train_dataloader = create_dataloader(train_file, **loader_config, shuffle=True)
+    train_dataloader = create_dataloader(args.train_file, **loader_config, shuffle=True)
     eval_dataloader = None
-    if eval_file:
-        eval_dataloader = create_dataloader(eval_file, **loader_config, shuffle=False)
+    if args.eval_file:
+        eval_dataloader = create_dataloader(args.eval_file, **loader_config, shuffle=False)
 
     word_embeddings = preprocessor.pretrained_word_embeddings
     if word_embeddings is not None:
@@ -56,34 +79,32 @@ def train(
     model = build_model(**model_config)
     model.to(device)
 
-    trainer = create_trainer(model, lr=lr, epoch=epochs)
+    trainer = create_trainer(model, lr=args.lr, epoch=args.epochs)
     trainer.add_callback(utils.training.PrintCallback(printer=logger.info))
     if eval_dataloader:
         rel_map = {v: k for k, v in preprocessor.vocabs["rel"].mapping.items()}
-        trainer.add_callback(EvaluateCallback(eval_file, rel_map), priority=0)
-        if save_dir:
-            torch.save(preprocessor, os.path.join(save_dir, "preprocessor.pt"))
+        trainer.add_callback(EvaluateCallback(args.eval_file, rel_map), priority=0)
+        if args.save_dir:
+            torch.save(preprocessor, os.path.join(args.save_dir, "preprocessor.pt"))
             trainer.add_callback(
-                utils.training.SaveCallback(save_dir, monitor="eval/UAS", mode="max")
+                utils.training.SaveCallback(args.save_dir, monitor="eval/UAS", mode="max")
             )
     with logging_redirect_tqdm(loggers=[logger]):
         trainer.fit(train_dataloader, eval_dataloader)
 
 
-def evaluate(
-    eval_file, checkpoint_file, preprocessor_file, batch_size=5000, cuda=False, verbose=False
-):
-    device = torch.device("cuda" if cuda else "cpu")
+def evaluate(args):
+    device = torch.device("cuda" if args.cuda else "cpu")
 
-    preprocessor = torch.load(preprocessor_file)
+    preprocessor = torch.load(args.preprocessor_file)
     loader_config = dict(
         preprocessor=preprocessor,
-        batch_size=batch_size,
+        batch_size=args.batch_size,
         device=device,
     )
-    eval_dataloader = create_dataloader(eval_file, **loader_config, shuffle=False)
+    eval_dataloader = create_dataloader(args.eval_file, **loader_config, shuffle=False)
 
-    checkpoint = torch.load(checkpoint_file)
+    checkpoint = torch.load(args.checkpoint_file)
     model_config = dict(
         word_vocab_size=len(preprocessor.vocabs["word"]),
         pretrained_word_vocab_size=len(preprocessor.vocabs["word"]),
@@ -97,59 +118,10 @@ def evaluate(
     trainer = create_trainer(model)
     trainer.add_callback(utils.training.PrintCallback(printer=logger.info))
     rel_map = {v: k for k, v in preprocessor.vocabs["rel"].mapping.items()}
-    trainer.add_callback(EvaluateCallback(eval_file, rel_map, verbose), priority=0)
+    trainer.add_callback(EvaluateCallback(args.eval_file, rel_map, args.verbose), priority=0)
     with logging_redirect_tqdm(loggers=[logger]):
         trainer.evaluate(eval_dataloader)
 
 
 if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(dest="command")
-
-    subparser = subparsers.add_parser("train")
-    subparser.add_argument("--train_file", type=str, required=True, metavar="FILE")
-    subparser.add_argument("--eval_file", type=str, default=None, metavar="FILE")
-    subparser.add_argument("--embed_file", type=str, default=None, metavar="FILE")
-    subparser.add_argument("--epochs", type=int, default=300, metavar="NUM")
-    subparser.add_argument("--batch_size", type=int, default=5000, metavar="NUM")
-    subparser.add_argument("--lr", type=float, default=2e-3, metavar="VALUE")
-    subparser.add_argument("--cuda", action="store_true")
-    subparser.add_argument("--save_dir", type=str, default=None, metavar="DIR")
-    subparser.add_argument("--seed", type=int, default=None, metavar="VALUE")
-
-    subparser = subparsers.add_parser("evaluate")
-    subparser.add_argument("--eval_file", type=str, required=True, metavar="FILE")
-    subparser.add_argument("--checkpoint_file", "--ckpt", type=str, required=True, metavar="FILE")
-    subparser.add_argument(
-        "--preprocessor_file", "--proc", type=str, required=True, metavar="FILE"
-    )
-    subparser.add_argument("--batch_size", type=int, default=5000, metavar="NUM")
-    subparser.add_argument("--cuda", action="store_true")
-    subparser.add_argument("--verbose", "-v", action="store_true")
-
-    args = parser.parse_args()
-    if args.command == "train":
-        train(
-            args.train_file,
-            args.eval_file,
-            args.embed_file,
-            args.epochs,
-            args.batch_size,
-            args.lr,
-            args.cuda,
-            args.save_dir,
-            args.seed,
-        )
-    if args.command == "evaluate":
-        evaluate(
-            args.eval_file,
-            args.checkpoint_file,
-            args.preprocessor_file,
-            args.batch_size,
-            args.cuda,
-            args.verbose,
-        )
-    else:
-        parser.print_help()
+    main()
